207780f77120bdf4d1e36e6fe3cd87f2e7367eb3
Ty Lovejoy <ty@fromskytocenter.com>
2025-12-07 17:32:50 -0600

    refactor: use new queue registry for xp service

--- extensions/xp.py
+++ extensions/xp.py
@@ -4,14 +4,13 @@ from logging import getLogger
 from math import floor
 from typing import TYPE_CHECKING
 
-import msgspec
 from aio_pika.abc import AbstractIncomingMessage
 from discord import TextChannel, app_commands, utils
 from discord.ext import commands
 from genjipk_sdk.users import Notification
 from genjipk_sdk.xp import XP_AMOUNTS, XP_TYPES, XpGrantEvent, XpGrantRequest
 
-from extensions._queue_registry import register_queue_handler
+from extensions._queue_registry import queue_consumer
 from utilities import transformers
 from utilities.base import BaseService
 
@@ -35,91 +34,6 @@ class XPService(BaseService):
         xp_channel = self.bot.get_channel(self.bot.config.channels.updates.xp)
         assert isinstance(xp_channel, TextChannel)
         self.xp_channel = xp_channel
-
-    async def _process_xp_notification(self, xp_message: XpGrantEvent) -> None:
-        """Send XP gain notifications and handle rank/prestige side effects.
-
-        Posts a public XP gain notice (respecting user notification settings),
-        checks for rank and prestige changes via the API, grants lootbox keys,
-        updates Discord roles, and sends appropriate DMs and channel messages.
-
-        Args:
-            xp_message (XpGrantMQ): Result payload containing data for notifications.
-        """
-        user = self.guild.get_member(xp_message.user_id)
-        if not user:
-            return
-
-        multiplier = await self.bot.api.get_xp_multiplier()
-        amount = floor(xp_message.amount * multiplier)
-
-        await self.bot.notifications.notify_channel_default_to_no_ping(
-            self.xp_channel,
-            xp_message.user_id,
-            Notification.PING_ON_XP_GAIN,
-            f"<:_:976917981009440798> {user.display_name} has gained **{amount} XP** ({xp_message.type})!",
-        )
-
-        xp_data = await self.bot.api.get_xp_tier_change(xp_message.previous_amount, xp_message.new_amount)
-
-        if xp_data.rank_change_type:
-            old_rank = " ".join((xp_data.old_main_tier_name, xp_data.old_sub_tier_name))
-            new_rank = " ".join((xp_data.new_main_tier_name, xp_data.new_sub_tier_name))
-
-            await self.bot.api.grant_active_key_to_user(xp_message.user_id)
-            await self._update_xp_roles_for_user(
-                xp_message.user_id, xp_data.old_main_tier_name, xp_data.new_main_tier_name
-            )
-
-            await self.bot.notifications.notify_dm(
-                xp_message.user_id,
-                Notification.DM_ON_LOOTBOX_GAIN,
-                (
-                    f"Congratulations! You have ranked up to **{new_rank}**!\n"
-                    "[Log into the website to open your lootbox!](https://genji.pk/lootbox)"
-                ),
-            )
-
-            await self.bot.notifications.notify_channel_default_to_no_ping(
-                self.xp_channel,
-                xp_message.user_id,
-                Notification.PING_ON_COMMUNITY_RANK_UPDATE,
-                f"<:_:976468395505614858> {user.display_name} has ranked up! **{old_rank}** -> **{new_rank}**\n",
-            )
-
-        if xp_data.prestige_change:
-            for _ in range(15):
-                await self.bot.api.grant_active_key_to_user(xp_message.user_id)
-
-            old_rank = " ".join((xp_data.old_main_tier_name, xp_data.old_sub_tier_name))
-            new_rank = " ".join((xp_data.new_main_tier_name, xp_data.new_sub_tier_name))
-
-            await self._update_xp_roles_for_user(
-                xp_message.user_id, xp_data.old_main_tier_name, xp_data.new_main_tier_name
-            )
-            await self._update_xp_prestige_roles_for_user(
-                xp_message.user_id, xp_data.old_prestige_level, xp_data.new_prestige_level
-            )
-
-            await self.bot.notifications.notify_dm(
-                xp_message.user_id,
-                Notification.DM_ON_LOOTBOX_GAIN,
-                (
-                    f"Congratulations! You have prestiged up to **{xp_data.new_prestige_level}**!\n"
-                    "[Log into the website to open your 15 lootboxes!](https://genji.pk/lootbox)"
-                ),
-            )
-
-            await self.bot.notifications.notify_channel_default_to_no_ping(
-                self.xp_channel,
-                xp_message.user_id,
-                Notification.PING_ON_COMMUNITY_RANK_UPDATE,
-                (
-                    f"<:_:976468395505614858><:_:976468395505614858><:_:976468395505614858> "
-                    f"{user.display_name} has prestiged! "
-                    f"**Prestige {xp_data.old_prestige_level}** -> **Prestige {xp_data.new_prestige_level}**"
-                ),
-            )
 
     async def _update_xp_prestige_roles_for_user(
         self, user_id: int, old_prestige_level: int, new_prestige_level: int
@@ -189,30 +103,79 @@ class XPService(BaseService):
         data = XpGrantRequest(XP_AMOUNTS[xp_type], xp_type)
         await self.bot.api.grant_user_xp(user_id, data)
 
-    @register_queue_handler("api.xp.grant")
-    async def _process_xp_grant(self, message: AbstractIncomingMessage) -> None:
-        """Handle a RabbitMQ message to create a playtest thread.
+    @queue_consumer("api.xp.grant", struct_type=XpGrantEvent, idempotent=True)
+    async def _process_xp_grant(self, event: XpGrantEvent, _: AbstractIncomingMessage) -> None:
+        log.debug(f"[x] [RabbitMQ] Processing XP grant event: {event.user_id}")
+        user = self.guild.get_member(event.user_id)
+        if not user:
+            return
 
-        Decodes the message and initiates the playtest creation process via
-        `add_playtest`.
+        multiplier = await self.bot.api.get_xp_multiplier()
+        amount = floor(event.amount * multiplier)
 
-        Args:
-            message (AbstractIncomingMessage): The message containing the playtest creation payload.
+        await self.bot.notifications.notify_channel_default_to_no_ping(
+            self.xp_channel,
+            event.user_id,
+            Notification.PING_ON_XP_GAIN,
+            f"<:_:976917981009440798> {user.display_name} has gained **{amount} XP** ({event.type})!",
+        )
 
-        Raises:
-            msgspec.ValidationError: If the message body cannot be decoded to the expected format.
-        """
-        try:
-            struct = msgspec.json.decode(message.body, type=XpGrantEvent)
-            if message.headers.get("x-pytest-enabled", False):
-                log.debug("Pytest message received.")
-                return
+        xp_data = await self.bot.api.get_xp_tier_change(event.previous_amount, event.new_amount)
 
-            log.debug(f"[x] [RabbitMQ] Processing XP message: {struct.user_id}")
+        if xp_data.rank_change_type:
+            old_rank = " ".join((xp_data.old_main_tier_name, xp_data.old_sub_tier_name))
+            new_rank = " ".join((xp_data.new_main_tier_name, xp_data.new_sub_tier_name))
 
-            await self._process_xp_notification(struct)
-        except Exception as e:
-            raise e
+            await self.bot.api.grant_active_key_to_user(event.user_id)
+            await self._update_xp_roles_for_user(event.user_id, xp_data.old_main_tier_name, xp_data.new_main_tier_name)
+
+            await self.bot.notifications.notify_dm(
+                event.user_id,
+                Notification.DM_ON_LOOTBOX_GAIN,
+                (
+                    f"Congratulations! You have ranked up to **{new_rank}**!\n"
+                    "[Log into the website to open your lootbox!](https://genji.pk/lootbox)"
+                ),
+            )
+
+            await self.bot.notifications.notify_channel_default_to_no_ping(
+                self.xp_channel,
+                event.user_id,
+                Notification.PING_ON_COMMUNITY_RANK_UPDATE,
+                f"<:_:976468395505614858> {user.display_name} has ranked up! **{old_rank}** -> **{new_rank}**\n",
+            )
+
+        if xp_data.prestige_change:
+            for __ in range(15):
+                await self.bot.api.grant_active_key_to_user(event.user_id)
+
+            old_rank = " ".join((xp_data.old_main_tier_name, xp_data.old_sub_tier_name))
+            new_rank = " ".join((xp_data.new_main_tier_name, xp_data.new_sub_tier_name))
+
+            await self._update_xp_roles_for_user(event.user_id, xp_data.old_main_tier_name, xp_data.new_main_tier_name)
+            await self._update_xp_prestige_roles_for_user(
+                event.user_id, xp_data.old_prestige_level, xp_data.new_prestige_level
+            )
+
+            await self.bot.notifications.notify_dm(
+                event.user_id,
+                Notification.DM_ON_LOOTBOX_GAIN,
+                (
+                    f"Congratulations! You have prestiged up to **{xp_data.new_prestige_level}**!\n"
+                    "[Log into the website to open your 15 lootboxes!](https://genji.pk/lootbox)"
+                ),
+            )
+
+            await self.bot.notifications.notify_channel_default_to_no_ping(
+                self.xp_channel,
+                event.user_id,
+                Notification.PING_ON_COMMUNITY_RANK_UPDATE,
+                (
+                    f"<:_:976468395505614858><:_:976468395505614858><:_:976468395505614858> "
+                    f"{user.display_name} has prestiged! "
+                    f"**Prestige {xp_data.old_prestige_level}** -> **Prestige {xp_data.new_prestige_level}**"
+                ),
+            )
 
 
 class XPCog(commands.GroupCog, group_name="xp"):
